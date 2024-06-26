ef6014c9cb95bfdfd190f5f8fd789eea62633f72
Tomas Albanesi <tomas.albanesi2000@gmail.com>
2024-06-26 18:58:11 -0300

    Tests in file main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import atanet_tools.ata_bandwidth_monitor as bw
 
-bw.scan_bw_to_file(limit_time=10, path_csv="prueba.csv")
+#bw.scan_bw_to_file(limit_time=10, path_csv="prueba.csv")
+#bw.scan_bw_to_console(limit_time=10, show_datetime=True)
